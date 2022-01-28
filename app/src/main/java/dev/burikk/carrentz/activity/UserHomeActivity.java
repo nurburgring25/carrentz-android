712d134043460ea000f1829152f799581e658e1f
Muhammad Irfan <nurburgring25@gmail.com>
2022-01-28 23:11:51 +0700

    Update.

--- app/src/main/java/dev/burikk/carrentz/activity/UserHomeActivity.java
+++ app/src/main/java/dev/burikk/carrentz/activity/UserHomeActivity.java
@@ -5,11 +5,25 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.synnapps.carouselview.CarouselView;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import dev.burikk.carrentz.R;
 
 @SuppressLint("NonConstantResourceId")
 public class UserHomeActivity extends AppCompatActivity {
+    @BindView(R.id.carouselView)
+    public CarouselView carouselView;
+
+    public static final int[] PROMO_IMAGES = {
+            R.drawable.img_promo_1,
+            R.drawable.img_promo_2,
+            R.drawable.img_promo_3,
+            R.drawable.img_promo_4,
+            R.drawable.img_promo_5
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
